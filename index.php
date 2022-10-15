6ba12ac0bac9df6fd0e89f95e9b858732e7b7f9c
un-know-n <fa1thlezss@gmail.com>
2022-10-15 23:33:37 +0300

    Fix of autoloading problem on heroku

--- index.php
+++ index.php
@@ -3,5 +3,13 @@
 //Turn on strict mode
 declare (strict_types = 1);
 
+//Possible fix of heroku autoloading problem
+require_once "config/classes/DB.php";
+require_once "config/classes/View.php";
+require_once "config/classes/Router.php";
+require_once "models/Conferences.php";
+require_once "models/Countries.php";
+require_once "controllers/MainController.php";
+
 //Check the URL
 require_once 'config/routes.php';
