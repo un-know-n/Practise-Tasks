a1ea01dd2bfe73a6d6ea9713a8a242e60921920b
un-know-n <fa1thlezss@gmail.com>
2023-02-21 15:25:01 +0200

    ENGINEER-FORM: Refactor updates
    List of changes:
    1) Async email validation
    2) Imitate server call for techologies versions
    3) Unsubscription added
    4) Code formatted

--- engineer-form/src/app/components/engineer-form/engineer-form.component.ts
+++ engineer-form/src/app/components/engineer-form/engineer-form.component.ts
@@ -1,6 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnDestroy } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { BehaviorSubject, Observable, delay, map, of } from 'rxjs';
 import { composeFormObject } from 'src/app/utils/composeFormObject';
 
 import { HobbyDialogComponent } from './hobby-dialog/hobby-dialog.component';
@@ -15,35 +24,48 @@ export interface IHobby {
   templateUrl: './engineer-form.component.html',
   styleUrls: ['./engineer-form.component.css'],
 })
-export class EngineerFormComponent implements OnInit {
-  engineerForm!: FormGroup;
-  technologiesVersions = {
+export class EngineerFormComponent implements OnDestroy {
+  engineerForm: FormGroup;
+  technologyVersions$ = new BehaviorSubject<string[]>([]);
+  serverData = {
     angular: ['1.1.1', '1.2.1', '1.3.3'],
     react: ['2.1.2', '3.2.4', '4.3.1'],
     vue: ['3.3.1', '5.2.1', '5.1.3'],
   };
 
-  constructor(public dialog: MatDialog) {}
-
-  ngOnInit(): void {
-    this.engineerForm = new FormGroup({
-      firstName: new FormControl('', Validators.required),
-      lastName: new FormControl('', Validators.required),
-      dateOfBirth: new FormControl('', Validators.required),
-      framework: new FormControl('', Validators.required),
-      frameworkVersion: new FormControl('', Validators.required),
-      email: new FormControl('', [
-        Validators.required,
-        Validators.pattern(
-          /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/g,
-        ),
-      ]),
-      hobbies: new FormControl<IHobby[]>([], Validators.required),
+  constructor(public dialog: MatDialog, private fb: FormBuilder) {
+    this.engineerForm = this.fb.group({
+      firstName: ['Some', Validators.required],
+      lastName: ['Name', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      framework: ['', Validators.required],
+      frameworkVersion: ['', Validators.required],
+      email: [
+        'test@test.testt',
+        [
+          Validators.required,
+          Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g),
+        ],
+        this.asyncEmailValidator,
+      ],
+      hobbies: [[{ name: 'sport', duration: '2 month' }], Validators.required],
     });
-    this.listenToEmailChange();
   }
 
-  onHobbyAdd() {
+  ngOnDestroy(): void {
+    this.technologyVersions$.unsubscribe();
+  }
+
+  onSubmit(formDirective: FormGroupDirective): void {
+    if (!formDirective.valid) {
+      console.log('There are errors still');
+      return;
+    }
+
+    console.log(composeFormObject(formDirective.form));
+  }
+
+  onHobbyAdd(): void {
     const hobbyDialogRef = this.dialog.open(HobbyDialogComponent, {
       height: 'auto',
       width: 'auto',
@@ -51,41 +73,39 @@ export class EngineerFormComponent implements OnInit {
 
     hobbyDialogRef.afterClosed().subscribe((result) => {
       if (result) {
-        this.engineerForm.controls['hobbies'].setValue([
-          ...this.engineerForm.controls['hobbies'].value,
-          result,
-        ]);
+        this.hobbies.setValue([...this.hobbies.value, result]);
       }
     });
   }
 
-  onSubmit() {
-    if (!this.engineerForm.valid) {
-      console.log('There are errors still');
-      return;
-    }
-
-    console.log(composeFormObject(this.engineerForm));
+  onDeleteHobby(index: number): void {
+    this.hobbies.setValue(this.hobbies.value.filter((_, i) => i !== index));
   }
 
-  onDeleteHobby(index: number) {
-    const filteredHobbies = (
-      this.engineerForm.controls['hobbies'].value as IHobby[]
-    ).filter((_, i) => i !== index);
-    this.engineerForm.controls['hobbies'].setValue(filteredHobbies);
-  }
-
-  listenToEmailChange() {
-    this.engineerForm.controls['email'].valueChanges.subscribe(
-      (value) =>
-        value === 'test@test.test' &&
-        this.engineerForm.controls['email'].setErrors({ invalid: true }),
+  asyncEmailValidator(
+    control: FormControl,
+  ): Observable<ValidationErrors | null> {
+    return of(control.value).pipe(
+      delay(300),
+      map((value) => (value === 'test@test.test' ? { invalid: true } : null)),
     );
   }
 
-  takeTechnologyVersions(string: any) {
-    return this.technologiesVersions[
-      string as keyof typeof this.technologiesVersions
-    ];
+  setTechnologyVersion(framework: string): void {
+    of(this.serverData)
+      .pipe(map((data) => data[framework as keyof typeof this.serverData]))
+      .subscribe((value) => this.technologyVersions$.next(value));
+  }
+
+  get framework(): AbstractControl<string> {
+    return <FormControl>this.engineerForm.get('framework');
+  }
+
+  get email(): AbstractControl<string> {
+    return <FormControl>this.engineerForm.get('email');
+  }
+
+  get hobbies(): AbstractControl<IHobby[]> {
+    return <FormControl>this.engineerForm.get('hobbies');
   }
 }
--- engineer-form/src/app/utils/composeFormObject.ts
+++ engineer-form/src/app/utils/composeFormObject.ts
@@ -3,7 +3,13 @@ import { FormGroup } from '@angular/forms';
 export const composeFormObject = (form: FormGroup) => {
   const result = {};
   for (const property in form.controls) {
-    (result as any)[property] = form.controls[property].value;
+    let value = form.controls[property].value;
+    if (value instanceof Date) {
+      value = `${value.getDate()}-${
+        value.getMonth() + 1
+      }-${value.getFullYear()}`;
+    }
+    (result as any)[property] = value;
   }
   return result;
 };
