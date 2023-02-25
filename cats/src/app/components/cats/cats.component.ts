d34491c7c59c697b705a2e88d66d6a6c61aae4a9
un-know-n <fa1thlezss@gmail.com>
2023-02-25 13:41:46 +0200

    CATS: Changed filter options

--- cats/src/app/components/cats/cats.component.ts
+++ cats/src/app/components/cats/cats.component.ts
@@ -1,22 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  AbstractControl,
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { map } from 'rxjs';
 import { AppState } from 'src/app/state/app.state';
 import { loadCats } from 'src/app/state/cats/cats.actions';
-import { Breed } from 'src/app/state/cats/cats.model';
-import {
-  selectAllBreeds,
-  selectAllCats,
-  selectError,
-  selectStatus,
-} from 'src/app/state/cats/cats.selectors';
+import { selectAllBreeds, selectAllCats, selectError, selectStatus } from 'src/app/state/cats/cats.selectors';
 
 @Component({
   selector: 'app-cats',
@@ -50,28 +38,20 @@ export class CatsComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.catsForm);
-
-    if (this.catsForm.valid) {
-      this.store.dispatch(
-        loadCats({ limit: this.limit.value, breed: this.breed.value[0].id }),
-      );
-    }
-  }
-
-  getError(control: string, error = 'required') {
-    return this.catsForm.controls[control].hasError(error);
+    this.store.dispatch(
+      loadCats({ limit: this.limit.value, breed: this.breed.value[0].id }),
+    );
   }
 
   checkStatus(status: ReturnType<typeof selectStatus>) {
     return this.status$.pipe(map((value) => value === status));
   }
 
-  get limit(): AbstractControl<string> {
+  get limit() {
     return <FormControl>this.catsForm.get('limit');
   }
 
-  get breed(): AbstractControl<Breed[]> {
-    return <FormControl>this.catsForm.get('breed');
+  get breed() {
+    return <FormArray>this.catsForm.get('breed');
   }
 }
