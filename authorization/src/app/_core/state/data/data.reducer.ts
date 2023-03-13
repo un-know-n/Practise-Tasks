9a5471096e5939e53d4df89f40c02abaf1fd000b
un-know-n <fa1thlezss@gmail.com>
2023-03-13 19:50:41 +0200

    AUTH: Data reducer cleaned

--- authorization/src/app/_core/state/data/data.reducer.ts
+++ authorization/src/app/_core/state/data/data.reducer.ts
@@ -12,11 +12,6 @@ export interface DataState {
   dashboard: IDashboardItem[];
   assessment: IAssessmentReport | null;
   error: string | null;
-  loading: {
-    dashboard: boolean;
-    assessment: boolean;
-    users: boolean;
-  };
 }
 
 export const initialState: DataState = {
@@ -24,71 +19,37 @@ export const initialState: DataState = {
   dashboard: [],
   users: [],
   error: null,
-  loading: {
-    assessment: false,
-    dashboard: false,
-    users: false,
-  },
 };
 
 export const dataReducer = createReducer(
   initialState,
   on(DataActions.loadAssessment, (state) => ({
     ...state,
-    loading: {
-      ...state.loading,
-      assessment: true,
-    },
     error: null,
   })),
   on(DataActions.loadAssessmentSuccess, (state, { assessment }) => ({
     ...state,
     assessment: assessment,
-    loading: {
-      ...state.loading,
-      assessment: false,
-    },
   })),
 
   on(DataActions.loadDashboard, (state) => ({
     ...state,
-    loading: {
-      ...state.loading,
-      dashboard: true,
-    },
     error: null,
   })),
   on(DataActions.loadDashboardSuccess, (state, { dashboard }) => ({
     ...state,
     dashboard: dashboard,
-    loading: {
-      ...state.loading,
-      dashboard: false,
-    },
   })),
   on(DataActions.loadUsers, (state) => ({
     ...state,
-    loading: {
-      ...state.loading,
-      users: true,
-    },
     error: null,
   })),
   on(DataActions.loadUsersSuccess, (state, { users }) => ({
     ...state,
     users: users,
-    loading: {
-      ...state.loading,
-      users: false,
-    },
   })),
   on(DataActions.loadError, (state, { error }) => ({
     ...state,
     error: error,
-    loading: {
-      assessment: false,
-      dashboard: false,
-      users: false,
-    },
   })),
 );
