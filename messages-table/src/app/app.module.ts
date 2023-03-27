71ffdd892d47a7b44f8def4da138b31ab6f9aaf3
un-know-n <fa1thlezss@gmail.com>
2023-03-27 23:36:20 +0300

    MSG: Initial project setup

---
+++ messages-table/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ messages-table/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'messages-table',
+    appId: '1:313277082324:web:17a0e448f9a4752f486941',
+    storageBucket: 'messages-table.appspot.com',
+    apiKey: 'AIzaSyBRNBJ5sBteUAN702dIsLyd_s69Sv_jRf4',
+    authDomain: 'messages-table.firebaseapp.com',
+    messagingSenderId: '313277082324',
+  },};
